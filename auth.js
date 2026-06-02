184c281ac33d3926c34537a80438e10e45390c93
yhsprice <yhsprice@hotmail.com>
2026-06-02 14:21:16 -0400

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,15 @@
+function getCurrentUser() {
+  const user = localStorage.getItem("loggedInUser");
 
+  if (!user) {
+    window.location.href = "login.html";
+    return null;
+  }
+
+  return JSON.parse(user);
+}
+
+function logout() {
+  localStorage.removeItem("loggedInUser");
+  window.location.href = "login.html";
+}
