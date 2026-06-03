f6856e3f8f3e45641fcaa4fa747d3c4b7aaa7393
yhsprice <yhsprice@hotmail.com>
2026-06-02 22:57:07 -0400

    Add releaseRequestList element to manager.js

--- manager.js
+++ manager.js
@@ -1,5 +1,7 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbwP9QdngM5iBsdCXWi8_p1_MyzkaTIo-m87TZcIvG9sVOcWoeanaVJbcDnanhr9g_-0mA/exec";
 
+const releaseRequestList = document.getElementById("releaseRequestList");
+
 const requestList = document.getElementById("requestList");
 
 const payApprovalList = document.getElementById("payApprovalList");
