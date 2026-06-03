e973140b5853d143cd8fb4cd834a8deed4ac28c8
yhsprice <yhsprice@hotmail.com>
2026-06-03 13:22:59 -0400

    Update payroll.js

--- payroll.js
+++ payroll.js
@@ -1,4 +1,4 @@
-https://script.google.com/macros/s/AKfycbztmN1-FfXwhUsmmRqseDW2rr8-DIUYUUENM5J7kJBZN0xrSIkfTTbZqXAFhh5qO0Xv/exec
+const API_URL = "https://script.google.com/macros/s/AKfycbztmN1-FfXwhUsmmRqseDW2rr8-DIUYUUENM5J7kJBZN0xrSIkfTTbZqXAFhh5qO0Xv/exec";
 
 const totalsContainer = document.getElementById("payrollTotals");
 const container = document.getElementById("payrollSummary");
