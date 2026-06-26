346d9615170cf9e4565a9dc1f7d2670587a6b367
yhsprice <yhsprice@hotmail.com>
2026-06-26 11:55:36 -0400

    Update auth.js

--- auth.js
+++ auth.js
@@ -69,3 +69,18 @@ function formatDateOnly(value) {
 
   return `${month}/${day}/${year}`;
 }
+
+function formatDateTime(value) {
+  if (!value) return "";
+
+  const date = new Date(value);
+  if (isNaN(date)) return value;
+
+  return date.toLocaleString("en-US", {
+    month: "2-digit",
+    day: "2-digit",
+    year: "numeric",
+    hour: "numeric",
+    minute: "2-digit"
+  });
+}
